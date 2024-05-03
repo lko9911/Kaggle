2b18db49652d0c8d61d501f66228d36449078af2
lko9911 <160494158+lko9911@users.noreply.github.com>
2024-05-03 18:36:01 +0900

    Update Compare_Regression_models.py

--- Compare_Regression_models.py
+++ Compare_Regression_models.py
@@ -104,17 +104,99 @@ def evaluate_models_RGXL(X, y):
 
     return results_df
 
-def plot_feature_importance(model_name, model, n_features, feature_names):
-    if hasattr(model, 'feature_importances_'):
-        importances = model.feature_importances_
-        indices = np.argsort(importances)[-n_features:]
-        plt.figure(figsize=(12, 6))
-        sns.barplot(y=feature_names[indices], x=importances[indices], palette="viridis")
-        plt.title(f'Top {n_features} Feature Importances for {model_name}', fontsize=18)
-        plt.xticks(rotation=0, fontsize=12)
-        plt.xlabel('Feature', fontsize=14)
-        plt.ylabel('Importance', fontsize=14)
-        plt.grid(linestyle='--')
-        plt.show()
-    else:
-        print(f"Model {model_name} does not support feature importance.")
+def evaluate_models_X(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    models = [
+        ('XGBoost Regressor', XGBRegressor())
+    ]
+
+    pipelines = {name: Pipeline([('scaler', StandardScaler()), (name, model)]) for name, model in models}
+
+    results = {}
+    trained_models = {}
+    for name, pipeline in pipelines.items():
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred, squared=False)
+        r2 = r2_score(y_test, y_pred)
+        results[name] = {'RMSE': rmse, 'R²': r2}
+        trained_models[name] = pipeline.named_steps[name]
+
+    results_df = pd.DataFrame.from_dict(results, orient='index').reset_index()
+    results_df.rename(columns={'index': 'Model'}, inplace=True)
+
+    return results_df
+
+def evaluate_models_L(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    models = [
+        ('LightGBM Regressor', LGBMRegressor())
+    ]
+
+    pipelines = {name: Pipeline([('scaler', StandardScaler()), (name, model)]) for name, model in models}
+
+    results = {}
+    trained_models = {}
+    for name, pipeline in pipelines.items():
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred, squared=False)
+        r2 = r2_score(y_test, y_pred)
+        results[name] = {'RMSE': rmse, 'R²': r2}
+        trained_models[name] = pipeline.named_steps[name]
+
+    results_df = pd.DataFrame.from_dict(results, orient='index').reset_index()
+    results_df.rename(columns={'index': 'Model'}, inplace=True)
+
+    return results_df
+
+def evaluate_models_R(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    models = [
+        ('Random Forest Regressor', RandomForestRegressor())
+    ]
+
+    pipelines = {name: Pipeline([('scaler', StandardScaler()), (name, model)]) for name, model in models}
+
+    results = {}
+    trained_models = {}
+    for name, pipeline in pipelines.items():
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred, squared=False)
+        r2 = r2_score(y_test, y_pred)
+        results[name] = {'RMSE': rmse, 'R²': r2}
+        trained_models[name] = pipeline.named_steps[name]
+
+    results_df = pd.DataFrame.from_dict(results, orient='index').reset_index()
+    results_df.rename(columns={'index': 'Model'}, inplace=True)
+
+    return results_df
+
+def evaluate_models_G(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    models = [
+        ('Gradient Boosting Regressor', GradientBoostingRegressor())
+    ]
+
+    pipelines = {name: Pipeline([('scaler', StandardScaler()), (name, model)]) for name, model in models}
+
+    results = {}
+    trained_models = {}
+    for name, pipeline in pipelines.items():
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        rmse = mean_squared_error(y_test, y_pred, squared=False)
+        r2 = r2_score(y_test, y_pred)
+        results[name] = {'RMSE': rmse, 'R²': r2}
+        trained_models[name] = pipeline.named_steps[name]
+
+    results_df = pd.DataFrame.from_dict(results, orient='index').reset_index()
+    results_df.rename(columns={'index': 'Model'}, inplace=True)
+
+    return results_df
+
